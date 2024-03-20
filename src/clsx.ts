14f349ab9ddd63e070fff42b6ccc4d2281625dce
mancuoj <mancuoj@gmail.com>
2024-03-20 22:21:02 +0800

    refactor: just if

--- src/clsx.ts
+++ src/clsx.ts
@@ -3,9 +3,11 @@ import type { ClassValue } from './types'
 function toVal(input: ClassValue): string | number {
   if (typeof input === 'string' || typeof input === 'number')
     return input
-  else if (Array.isArray(input))
+
+  if (Array.isArray(input))
     return input.map(toVal).filter(Boolean).join(' ')
-  else if (typeof input === 'object' && input !== null)
+
+  if (typeof input === 'object' && input !== null)
     return Object.keys(input).filter(key => Boolean(input[key])).join(' ')
 
   return ''
